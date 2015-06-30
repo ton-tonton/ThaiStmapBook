47ed24e2777af5f87175b3ee9eed8b1211f893e4
Ton <phantom.ness@windowslive.com>
2015-06-30 15:06:19 +0700

    add Cover image to HomeViewController

--- ThaiStampBook/HomeViewController.swift
+++ ThaiStampBook/HomeViewController.swift
@@ -8,18 +8,64 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+let year = ["1968", "1972", "1973", "1975", "1976", "1968", "1972", "1973", "1975", "1976", "1968", "1972", "1973", "1975", "1976"]
+let NAVBAR_CHANGE_POINT: CGFloat = 8.0
 
+class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        UIApplication.sharedApplication().statusBarStyle = UIStatusBarStyle.LightContent
+        self.navigationController?.navigationBar.lt_setBackgroundColor(UIColor.clearColor())
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(true)
+        
+        self.scrollViewDidScroll(self.tableView)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        let color = UIColor(red: 0/255.0, green: 175/255.0, blue: 240/255.0, alpha: 1)
+        let offsetY = scrollView.contentOffset.y
+        let navigationBar = self.navigationController?.navigationBar
+        
+        if offsetY > NAVBAR_CHANGE_POINT {
+            var alpha = 1 - ((NAVBAR_CHANGE_POINT + 64 - offsetY) / 64)
+            navigationBar?.lt_setBackgroundColor(color.colorWithAlphaComponent(alpha))
+            
+        } else {
+            navigationBar?.lt_setBackgroundColor(color.colorWithAlphaComponent(0))
+        }
+    }
+    
+    // MARK: - TableViewDataSource
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return year.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as? UITableViewCell
+        let row = indexPath.row
+        cell?.textLabel?.text = year[row]
+        return cell!
+    }
+    
+    // MARK: - TableViewDelegate
     
 
     /*
